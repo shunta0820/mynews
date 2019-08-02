b8f84186d7f302c267fd6ea2a6df433fa5395b5c
”shunta <shuntatoukairin1204@gmail.com>
2019-08-02 03:54:03 +0000

    eleventh commit

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -11,7 +11,7 @@ class ProfileController extends Controller
   {
       return view('admin.profile.create');
   }
-
+  
  
 
  public function edit()
--- resources/views/admin/news/index.blade.php
+++ resources/views/admin/news/index.blade.php
@@ -48,8 +48,8 @@
                                        <a href="{{ action('Admin\NewsController@edit', ['id' => $news->id]) }}">編集</a>
                                        </div>
                                        <div>
-                                          <a href="{{ action('Admin\NewsController@delete', ['id' => $news->id]) }}">削除</a>
-                                       </div>
+                                            <a href="{{ action('Admin\NewsController@delete', ['id' => $news->id]) }}">削除</a>
+                                    　　</div>
                                     </td>
                                 </tr>
                             @endforeach
